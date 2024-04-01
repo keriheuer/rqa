c0275da092c8d2334f384868bdb26b438afff695
Keri Heuer <keriheuer@Keris-MacBook-Pro-2.local>
2024-03-31 23:31:00 -0400

    Changed order of imports in config.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,11 @@
 
 __name__ = 'rqa'
 
+from . import config
+from . import utils
 from .characteristic_rqa import CharacteristicRQA
 from .generate_rps import GenerateRPs
 from .combine_timeseries import CombineTimeseries
 # from .embedding import EmbeddingParameters
 from . import timeseries
-from . import utils
-from . import config
+
--- src/rqa/config.py
+++ src/rqa/config.py
@@ -7,10 +7,11 @@ import sys
 import pkg_resources
 import subprocess
 
-### MODULES ###
+### WIDGETS ####
 
-from .utils import *
-from .pyunicorn.timeseries import RecurrencePlot
+from IPython.display import display, Javascript
+from ipywidgets import Layout, HBox, Box, VBox, IntSlider, FloatSlider, Dropdown, ToggleButton, ToggleButtons, Label, HTML
+import ipyevents
 
 ### PLOTTING ###
 
@@ -22,20 +23,14 @@ from matplotlib.backend_tools import Cursors
 from matplotlib.text import Annotation
 from mpl_toolkits.mplot3d.proj3d import proj_transform
 from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import to_rgb
 
-import matplotlib as mpl
-import matplotlib.font_manager as fm
-font= fm.FontEntry( fname=get_resource_path("data/cmunrm.ttf"), name='cmunrm')
-fm.fontManager.ttflist.insert(0, font)
-mpl.rcParams['font.family'] = font.name
+### MODULES ###
 
-set_matplotlib_formats('svg')
-mpl.style.use(get_resource_path("data/standard.mplstyle"))
+from .utils import *
+from pyunicorn.timeseries import RecurrencePlot
 
-### WIDGETS ####
-
-from IPython.display import display, Javascript
-from ipywidgets import Layout, HBox, Box, VBox, IntSlider, FloatSlider, Dropdown, ToggleButton, ToggleButtons, Label, HTML
 
 #### VARIABLES ####
 
@@ -71,5 +66,15 @@ for i, s in enumerate(systems):
     for stat, val in zip(df.columns, df.iloc[i].values):
         characteristic_rqa_stats[s][stat] = val
       
-### SETUP ###  
+### STYLES ###  
+
 setup_notebook()
+
+import matplotlib as mpl
+import matplotlib.font_manager as fm
+font= fm.FontEntry( fname=get_resource_path("data/cmunrm.ttf"), name='cmunrm')
+fm.fontManager.ttflist.insert(0, font)
+mpl.rcParams['font.family'] = font.name
+
+set_matplotlib_formats('svg')
+mpl.style.use(get_resource_path("data/standard.mplstyle"))
